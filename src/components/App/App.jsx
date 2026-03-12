65b762470361460f52a510de904ede1b81402df8
webDeveloper42 <rfrias361@gmail.com>
2026-03-12 15:31:20 -0400

    Fix:add name to radio buttons

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -58,19 +58,19 @@ function App() {
           <label htmlFor="name" className="modal__label">
             Name{" "}
             <input
-              type="text "
+              type="text"
               className="modal__input"
               id="name"
               placeholder="Name"
             />
           </label>
-          <label htmlFor="imageUrl" className="modal__label">
+          <label htmlFor="Image URL" className="modal__label">
             Image{" "}
             <input
-              type="url "
+              type="url"
               className="modal__input"
-              id="imageUrl"
-              placeholder="Image Url"
+              id="Image URL"
+              placeholder="Image URL"
             />
           </label>
           <fieldset className="modal__radio-buttons">
@@ -79,21 +79,36 @@ function App() {
               htmlFor="hot"
               className="modal__label modal__label_type_radio"
             >
-              <input id="hot" type="radio" className="modal__radio-input " />{" "}
+              <input
+                name="weather"
+                id="hot"
+                type="radio"
+                className="modal__radio-input "
+              />{" "}
               Hot
             </label>
             <label
               htmlFor="warm"
               className="modal__label modal__label_type_radio"
             >
-              <input id="warm" type="radio" className="modal__radio-input " />{" "}
+              <input
+                name="weather"
+                id="warm"
+                type="radio"
+                className="modal__radio-input "
+              />{" "}
               Warm
             </label>
             <label
               htmlFor="cold"
               className="modal__label modal__label_type_radio"
             >
-              <input id="cold" type="radio" className="modal__radio-input " />{" "}
+              <input
+                name="weather"
+                id="cold"
+                type="radio"
+                className="modal__radio-input "
+              />{" "}
               Cold
             </label>
           </fieldset>
