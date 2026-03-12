b592d502e3466cf7ae3a153aafb6cf39a45a7666
webDeveloper42 <rfrias361@gmail.com>
2026-03-12 14:11:50 -0400

    Fix:Main css import typo

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import "./main.css";
+import "./Main.css";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import { defaultClothingItems } from "../../utils/constants";
 import ItemCard from "../ItemCard/ItemCard";
