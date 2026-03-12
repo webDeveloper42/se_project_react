30b9cb556623de2bb3de489ee333db4d033f4886
webDeveloper42 <rfrias361@gmail.com>
2026-03-12 14:50:48 -0400

    Fix: footer sub class typo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer_developer-rights">Developed by Raymond</p>
+      <p className="footer__developer-rights">Developed by Raymond</p>
       <p className="footer__year">2026</p>
     </footer>
   );
