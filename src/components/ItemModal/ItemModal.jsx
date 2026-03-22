6b8ef9150c063ed68167e14219cf8555363e5385
webDeveloper42 <rfrias361@gmail.com>
2026-03-22 18:48:23 -0400

    Feature: add delete button to card modal

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -20,7 +20,10 @@ function ItemModal({ activeModal, card, handleCloseClick }) {
           className="modal__image"
         />
         <div className="modal__footer">
-          <h2 className="modal__caption">{card.name}</h2>
+          <div className="modal__footer-heading">
+            <h2 className="modal__caption">{card.name}</h2>
+            <button className="modal__delete-btn">Delete Item</button>
+          </div>
           <p className="modal__weather">Weather: {card.weather}</p>
         </div>
       </div>
