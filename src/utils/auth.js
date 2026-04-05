b9b9642a35b37b258d7d36d36c8a2a820c08847f
webDeveloper42 <rfrias361@gmail.com>
2026-04-05 16:57:45 -0400

    Fix:auth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 import { checkRes } from "./api";
 
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 const signUp = ({ name, avatar, email, password }) => {
   return fetch(`${BASE_URL}/signup`, {
